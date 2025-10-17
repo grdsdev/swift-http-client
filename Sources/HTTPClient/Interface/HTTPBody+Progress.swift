d445d8d550134aace24b97194b41cd5a261b9399
Guilherme Souza <ogrsouza@gmail.com>
2025-10-17 15:16:19 -0300

    docs: add docs to progress tracking

--- Sources/HTTPClient/Interface/HTTPBody+Progress.swift
+++ Sources/HTTPClient/Interface/HTTPBody+Progress.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 extension HTTPBody {
+  /// Tracks the progress of the body and calls the handler.
   public func trackingProgress(handler: @escaping (Progress) -> Void) -> HTTPBody {
     var totalBytesProcessed: Int64 = 0
     let totalLength: Int64? =
@@ -24,10 +25,14 @@ extension HTTPBody {
   }
 }
 
+/// A progress object that tracks the progress of a body.
 public struct Progress: Sendable, Hashable {
+  /// The number of bytes processed.
   public let completed: Int64
+  /// The total number of bytes to process.
   public let total: Int64?
 
+  /// The fraction of the body that has been processed.
   public var fractionCompleted: Double? {
     guard let total = total, total > 0 else { return nil }
     return Double(completed) / Double(total)

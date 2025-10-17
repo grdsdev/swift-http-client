dc6e0f25e80ed47d3631d6002680878a842bc950
Guilherme Souza <ogrsouza@gmail.com>
2025-10-17 09:54:56 -0300

    docs: add docs for public multipart methods

--- Sources/HTTPClient/Interface/MultipartFormData+HTTPBody.swift
+++ Sources/HTTPClient/Interface/MultipartFormData+HTTPBody.swift
@@ -1,17 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the SwiftOpenAPIGenerator open source project
-//
-// Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-//===----------------------------------------------------------------------===//
-
 import Foundation
 import HTTPTypes
 
@@ -75,6 +61,7 @@ extension HTTPBody {
     let stream = AsyncThrowingStream<ByteChunk, any Error> { continuation in
 
       continuation.onTermination = { _ in
+        try? fileHandle.close()
         if deleteOnDeallocation {
           try? FileManager.default.removeItem(at: fileURL)
         }
